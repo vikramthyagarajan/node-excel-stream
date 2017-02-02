610772004456a8228927b6e5211452ea9d87c14d
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2017-02-02 16:50:14 +0530

    Passed all tests for reader

--- src/excel-reader.js
+++ src/excel-reader.js
@@ -46,7 +46,7 @@ class ExcelReader {
                 key: Joi.string(),
                 allowedNames: Joi.array().items(Joi.string()),
                 rows: Joi.object().keys({
-                    headerRow: Joi.number(),
+                    headerRow: Joi.number().min(1),
                     allowedHeaders: Joi.array().items(Joi.object().keys({
                         name: Joi.string(),
                         key: Joi.string()
@@ -76,10 +76,10 @@ class ExcelReader {
 		this.workbook.eachSheet((worksheet, sheetId) => {
             let name = worksheet.name;
             let sheetSchema = _.find(sheetOptions, {name: name});
-            let boolean, message = this._checkSheet(worksheet, sheetSchema);
-            if (!boolean) {
-                isValid = boolean;
-                lastMessage = message;
+            let result = this._checkSheet(worksheet, sheetSchema);
+            if (result.isValid === false) {
+                isValid = result.isValid;
+                lastMessage = result.message;
             }
 		});
 
@@ -94,14 +94,14 @@ class ExcelReader {
      */
     _checkSheet(worksheet, sheetOptions) {
         let isValid = true, lastMessage = '';
-        if (!sheetOptions || !sheetOptions.rows) {
+        if (!sheetOptions || !sheetOptions.rows || _.isEmpty(sheetOptions.rows)) {
             isValid = false;
             lastMessage = 'Schema not found for sheet ' + worksheet.name;
         }
         else if (_.isNumber(sheetOptions.rows.headerRow) && sheetOptions.rows.allowedHeaders) {
             let rowOptions = sheetOptions.rows;
             let row = worksheet.getRow(rowOptions.headerRow);
-            headerNames = _.remove(row.values, null);
+            let headerNames = _.remove(row.values, null);
 			const allowedHeaderNames = _.map(rowOptions.allowedHeaders, 'name');
 			if (!_.chain(headerNames).difference(allowedHeaderNames).isEmpty().value()) {
 				lastMessage = 'The row ' + rowOptions.headerRow + ' must contain headers. Only these header values are allowed: ' + allowedHeaderNames;
@@ -109,15 +109,25 @@ class ExcelReader {
 			}
 		}
 
-        return isValid, lastMessage;
+        return {
+            isValid: isValid,
+            message: lastMessage
+        };
     }
 
+    /** 
+     * Error that is cause because of incorrect data is inputted to the class
+     */
 	_dataError(message) {
 		return new Error(message);
 	}
 
+    /** 
+     * Error that is caused by the class itself, and is not related to the
+     * options provided by the user
+     */
 	_internalError(message) {
-		return new Error('error while parsing excel file' + message);
+		return new Error('error while parsing excel file: ' + message);
 	}
 
 	getDefaultSheet() {
@@ -129,9 +139,7 @@ class ExcelReader {
 
 	/**
 	 * Returns a json version of the row data, based on the
-	 * allowedHeaders of the default sheet. 
-	 * Caveat: The allowedHeader must have an index option beforehand.
-	 * This is to be calculated and set prior to calling this function.
+	 * allowedHeaders of the given sheet. 
 	 */
 	_getRowData(rowObject, rowNum, allowedHeaders, headerRowValues) {
 		let result = {};
@@ -142,46 +150,51 @@ class ExcelReader {
 			}
 			let header = headerRowValues[cellNo];
 			if (header) {
-				let currentHeader = _.find(allowedHeaders, {text: header});
+				let currentHeader = _.find(allowedHeaders, {name: header});
 				let cellValue = cell.value;
-				result[currentHeader.fieldName] = cellValue;
+                if (_.isObject(cell.value)) {
+                    // If this is an object, then a formula has been applied
+                    // We just take the result in that case
+                    cellValue = cell.value.result;
+                }
+				result[currentHeader.key] = cellValue;
 			}
 		});
 		return result;
 	}
 
 	/**
-	 * Takes a callback and runs it on every row of the default sheet.
-	 * Also, the default headers and header rows must be set, otherwise
-	 * just get iterate the worksheets and do what you want.
+	 * Takes a callback and runs it on every row of the every sheet, one by one.
+     * Order of the sheets is not guaranteed.
 	 * This method provides each row in a json format based on the headers picked
 	 * up from options
 	 * callback params-
 	 *  1. rowData, a json with key being the header field, picked up from options.row
 	 *  2. rowNum, counting the headerRow
+	 *  3. sheetKey, key of the sheet. If no key exists, the name is provided
 	 *  The callback must return a promise
 	 */
 	eachRow(callback) {
 		return this.afterRead.then(() => {
-			let defaultSheet = this.getDefaultSheet();
 			let rowPromises = [];
-			// if (!defaultSheet) {
-			// 	throw this._internalError('default sheet is not found');
-			// }
-			// if (!this.options.rows || !this.options.rows.headerRow || !this.options.rows.allowedHeaders) {
-			// 	throw this._internalError('headerRow and allowedHeaders not set. Do not use this function. Use this.workbook itself');
-			// }
-			// let allowedHeaders = this.options.rows.allowedHeaders;
-			// let headerRowValues = defaultSheet.getRow(this.options.rows.headerRow).values;
-			// defaultSheet.eachRow((row, rowNum) => {
-			// 	// ignoreing the headerRow
-			// 	if (rowNum == this.options.rows.headerRow) {
-			// 		return;
-			// 	}
-			// 	// processing the rest rows
-			// 	let rowData = this._getRowData(row, rowNum, allowedHeaders, headerRowValues);
-			// 	rowPromises.push(callback(rowData, rowNum));
-			// })
+            this.workbook.eachSheet((worksheet, sheetId) => {
+                let sheetName = worksheet.name;
+                let sheetOptions = _.find(this.options.sheets, {name: worksheet.name});
+                let sheetKey = sheetOptions.key ? sheetOptions.key: sheetName;
+                let headerRow = sheetOptions.rows.headerRow? sheetOptions.rows.headerRow: 1;
+                let allowedHeaders = sheetOptions.rows.allowedHeaders;
+                let headerRowValues = worksheet.getRow(headerRow).values;
+                worksheet.eachRow((row, rowNum) => {
+                    // ignoreing all the rows lesser than the headerRow
+                    if (rowNum <= headerRow) {
+                        return;
+                    }
+                    // processing the rest rows
+                    let normalizedRowNum = rowNum - headerRow;
+                    let rowData = this._getRowData(row, normalizedRowNum, allowedHeaders, headerRowValues);
+                    rowPromises.push(callback(rowData, normalizedRowNum, sheetKey));
+                });
+            });
 			return Promise.all(rowPromises);
 		});
 	}
